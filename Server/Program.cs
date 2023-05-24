185b197d2db5dfaf7ba466eb4abb66eaa1b36c51
selebry <murad1703@yandex.ru>
2023-05-24 17:52:06 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using TicTacToe;
+
+
+namespace TicTacToeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Tic-Tac-Toe Server");
+
+                Server server = new Server();
+                server.Start();
+
+                Console.WriteLine("Для остановки сервера нажмите любую клавишу.");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+    public class Server
+    {
+        private UdpClient udpServer;
+        private IPEndPoint client1EndPoint;
+        private IPEndPoint client2EndPoint;
+        private Dictionary<IPEndPoint, string> playerNames;
+        private Dictionary<IPEndPoint, PlayerSymbol> playerSymbols;
+        private IPEndPoint currentPlayerEndPoint;
+        private GameStatus gameStatus;
+        private GameBoard gameBoard;
+        private GameWindow gameWindow;
+
+        public Server()
+        {
+            playerNames = new Dictionary<IPEndPoint, string>();
+            playerSymbols = new Dictionary<IPEndPoint, PlayerSymbol>();
+            gameBoard = new GameBoard();
+            gameStatus = GameStatus.NotStarted;
+        }
+
+        public void Start()
+        {
+            udpServer = new UdpClient(new IPEndPoint(IPAddress.Any, 12345));
+            Console.WriteLine("Server started. Waiting for players...");
+
+            while (playerSymbols.Count < 2)
+            {
+                IPEndPoint clientEndPoint = null;
+                byte[] receivedBytes = udpServer.Receive(ref clientEndPoint);
+                string playerName = Encoding.ASCII.GetString(receivedBytes);
+
+                if (!playerNames.ContainsKey(clientEndPoint))
+                {
+                    playerNames.Add(clientEndPoint, playerName);
+                    playerSymbols.Add(clientEndPoint, playerSymbols.Count == 0 ? PlayerSymbol.Cross : PlayerSymbol.Circle);
+
+                    if (playerSymbols.Count == 1)
+                        client1EndPoint = clientEndPoint;
+                    else
+                        client2EndPoint = clientEndPoint;
+
+                    Console.WriteLine("Player connected: " + playerName);
+                }
+            }
+
+            gameStatus = GameStatus.InProgress;
+            currentPlayerEndPoint = client1EndPoint;
+            SendGameStateToClients();
+
+            Console.WriteLine("Game started. Players: " + playerNames[client1EndPoint] + " (X) vs " + playerNames[client2EndPoint] + " (O)");
+
+            while (gameStatus == GameStatus.InProgress)
+            {
+                IPEndPoint clientEndPoint = null;
+                byte[] receivedBytes = udpServer.Receive(ref clientEndPoint);
+
+                if (gameStatus == GameStatus.InProgress && currentPlayerEndPoint == clientEndPoint)
+                {
+                    int cellIndex = BitConverter.ToInt32(receivedBytes, 0);
+
+                    if (gameBoard.IsCellEmpty(cellIndex))
+                    {
+                        gameBoard.SetCellState(cellIndex, playerSymbols[clientEndPoint]);
+
+                        if (CheckWinCondition(playerSymbols[clientEndPoint]))
+                        {
+                            gameStatus = playerSymbols[clientEndPoint] == PlayerSymbol.Cross ? GameStatus.CrossWin : GameStatus.CircleWin;
+                            SendGameStateToClients();
+                            break;
+                        }
+                        else if (gameBoard.IsBoardFull())
+                        {
+                            gameStatus = GameStatus.Draw;
+                            SendGameStateToClients();
+                            break;
+                        }
+                        else
+                        {
+                            currentPlayerEndPoint = currentPlayerEndPoint == client1EndPoint ? client2EndPoint : client1EndPoint;
+                            SendGameStateToClients();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Game over. " + (gameStatus == GameStatus.Draw ? "Draw." : "Winner: " + playerNames[currentPlayerEndPoint]));
+            udpServer.Close();
+           // gameWindow.Close();
+        }
+
+        private void SendGameStateToClients()
+        {
+            byte[] gameStateBytes = GetGameStateBytes();
+            udpServer.Send(gameStateBytes, gameStateBytes.Length, client1EndPoint);
+            udpServer.Send(gameStateBytes, gameStateBytes.Length, client2EndPoint);
+
+            //if (gameWindow != null)
+            //    gameWindow.UpdateGameBoard(gameBoard);
+        }
+
+        private byte[] GetGameStateBytes()
+        {
+            byte[] gameStateBytes = new byte[626];
+            int index = 0;
+
+            gameStateBytes[index++] = (byte)gameStatus;
+
+            foreach (KeyValuePair<IPEndPoint, string> player in playerNames)
+            {
+                byte[] playerNameBytes = Encoding.ASCII.GetBytes(player.Value);
+                Buffer.BlockCopy(playerNameBytes, 0, gameStateBytes, index, playerNameBytes.Length);
+                index += 100;
+            }
+
+            foreach (KeyValuePair<IPEndPoint, PlayerSymbol> player in playerSymbols)
+            {
+                gameStateBytes[index++] = (byte)player.Value;
+            }
+
+            for (int i = 0; i < GameBoard.Size; i++)
+            {
+                gameStateBytes[index++] = (byte)gameBoard.GetCellState(i);
+            }
+
+            return gameStateBytes;
+        }
+
+        private bool CheckWinCondition(PlayerSymbol symbol)
+        {
+            // Check horizontal lines
+            for (int row = 0; row < GameBoard.Size; row++)
+            {
+                for (int col = 0; col <= GameBoard.Size - 5; col++)
+                {
+                    bool win = true;
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (gameBoard.GetCellState(row * GameBoard.Size + col + i) != symbol)
+                        {
+                            win = false;
+                            break;
+                        }
+                    }
+
+                    if (win)
+                        return true;
+                }
+            }
+
+            // Check vertical lines
+            for (int col = 0; col < GameBoard.Size; col++)
+            {
+                for (int row = 0; row <= GameBoard.Size - 5; row++)
+                {
+                    bool win = true;
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (gameBoard.GetCellState((row + i) * GameBoard.Size + col) != symbol)
+                        {
+                            win = false;
+                            break;
+                        }
+                    }
+
+                    if (win)
+                        return true;
+                }
+            }
+
+            // Check main diagonal lines
+            for (int row = 0; row <= GameBoard.Size - 5; row++)
+            {
+                for (int col = 0; col <= GameBoard.Size - 5; col++)
+                {
+                    bool win = true;
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (gameBoard.GetCellState((row + i) * GameBoard.Size + col + i) != symbol)
+                        {
+                            win = false;
+                            break;
+                        }
+                    }
+
+                    if (win)
+                        return true;
+                }
+            }
+
+            // Check anti-diagonal lines
+            for (int row = 4; row < GameBoard.Size; row++)
+            {
+                for (int col = 0; col <= GameBoard.Size - 5; col++)
+                {
+                    bool win = true;
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (gameBoard.GetCellState((row - i) * GameBoard.Size + col + i) != symbol)
+                        {
+                            win = false;
+                            break;
+                        }
+                    }
+
+                    if (win)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private UdpClient udpClient;
+        private IPEndPoint serverEndPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serverIP = ServerIPTextBox.Text;
+            int serverPort;
+
+            if (IPAddress.TryParse(serverIP, out IPAddress ipAddress) && int.TryParse(ServerPortTextBox.Text, out serverPort))
+            {
+                serverEndPoint = new IPEndPoint(ipAddress, serverPort);
+                udpClient = new UdpClient();
+
+                byte[] playerNameBytes = Encoding.ASCII.GetBytes(PlayerNameTextBox.Text);
+                udpClient.Send(playerNameBytes, playerNameBytes.Length, serverEndPoint);
+
+                GameWindow gameWindow = new GameWindow(udpClient, serverEndPoint);
+                gameWindow.Show();
+
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid server IP or port.");
+            }
+        }
+    }
+
+    public enum PlayerSymbol
+    {
+        Cross,
+        Circle,
+        None
+    }
+
+    public enum GameStatus
+    {
+        NotStarted,
+        InProgress,
+        CrossWin,
+        CircleWin,
+        Draw
+    }
+
+    public partial class GameWindow : Window
+    {
+        private UdpClient udpClient;
+        private IPEndPoint serverEndPoint;
+        private Button[] cellButtons;
+        private PlayerSymbol playerSymbol;
+
+        public GameWindow(UdpClient client, IPEndPoint endPoint)
+        {
+            InitializeComponent();
+            udpClient = client;
+            serverEndPoint = endPoint;
+            cellButtons = new Button[625];
+            playerSymbol = PlayerSymbol.Circle;
+
+            for (int i = 0; i < 625; i++)
+            {
+                cellButtons[i] = new Button();
+                cellButtons[i].Content = "";
+                cellButtons[i].Tag = i;
+                cellButtons[i].Click += CellButton_Click;
+                GameBoardGrid.Children.Add(cellButtons[i]);
+            }
+
+            UpdateGameBoard(new GameBoard());
+            StartReceivingUpdates();
+        }
+
+        private void CellButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (playerSymbol == PlayerSymbol.Circle)
+            {
+                Button button = (Button)sender;
+                int cellIndex = (int)button.Tag;
+
+                byte[] cellIndexBytes = BitConverter.GetBytes(cellIndex);
+                udpClient.Send(cellIndexBytes, cellIndexBytes.Length, serverEndPoint);
+            }
+        }
+
+        private void StartReceivingUpdates()
+        {
+            udpClient.BeginReceive(ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult result)
+        {
+            IPEndPoint remoteEndPoint = null;
+            byte[] receivedBytes = udpClient.EndReceive(result, ref remoteEndPoint);
+
+            if (receivedBytes.Length == 626)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    GameStatus gameStatus = (GameStatus)receivedBytes[0];
+                    int index = 1;
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        string playerName = Encoding.ASCII.GetString(receivedBytes, index, 100).TrimEnd('\0');
+                        index += 100;
+                        SetPlayerName(i, playerName);
+                    }
+
+                    //for (int i = 0; i < 625; i++)
+                    //{
+                    //    SetCellState(i, (PlayerSymbol)receivedBytes[index++]);
+                    //}
+
+                    if (gameStatus == GameStatus.InProgress && playerSymbol == PlayerSymbol.Circle)
+                        playerSymbol = PlayerSymbol.Cross;
+
+                    if (gameStatus != GameStatus.NotStarted)
+                    {
+                        GameStatusLabel.Text = GetGameStatusLabel(gameStatus);
+                        DisableCellButtons();
+                    }
+                });
+            }
+
+            StartReceivingUpdates();
+        }
+
+        private void SetPlayerName(int playerIndex, string playerName)
+        {
+            switch (playerIndex)
+            {
+                case 0:
+                    Player1NameLabel.Text = playerName;
+                    break;
+                case 1:
+                    Player2NameLabel.Text = playerName;
+                    break;
+            }
+        }
+
+        private void SetCellState(int cellIndex, PlayerSymbol symbol)
+        {
+            cellButtons[cellIndex].Content = symbol == PlayerSymbol.Cross ? "X" : "O";
+            cellButtons[cellIndex].IsEnabled = symbol == PlayerSymbol.Cross ? false : true;
+        }
+
+        private void DisableCellButtons()
+        {
+            for (int i = 0; i < 625; i++)
+            {
+                cellButtons[i].IsEnabled = false;
+            }
+        }
+
+        private string GetGameStatusLabel(GameStatus status)
+        {
+            switch (status)
+            {
+                case GameStatus.NotStarted:
+                    return "Game not started.";
+                case GameStatus.InProgress:
+                    return "Game in progress.";
+                case GameStatus.CrossWin:
+                    return "Cross wins!";
+                case GameStatus.CircleWin:
+                    return "Circle wins!";
+                case GameStatus.Draw:
+                    return "It's a draw.";
+                default:
+                    return "";
+            }
+        }
+
+        public void UpdateGameBoard(GameBoard gameBoard)
+        {
+            for (int i = 0; i < 625; i++)
+            {
+                SetCellState(i, gameBoard.GetCellState(i));
+            }
+        }
+    }
+
+    public class GameBoard
+    {
+        public const int Size = 25;
+        private PlayerSymbol[] cells;
+
+        public GameBoard()
+        {
+            cells = new PlayerSymbol[625];
+        }
+
+        public PlayerSymbol GetCellState(int index)
+        {
+            return cells[index];
+        }
+
+        public void SetCellState(int index, PlayerSymbol symbol)
+        {
+            cells[index] = symbol;
+        }
+
+        public bool IsCellEmpty(int index)
+        {
+            return cells[index] == PlayerSymbol.Cross || cells[index] == PlayerSymbol.Circle;
+        }
+
+        public bool IsBoardFull()
+        {
+            foreach (PlayerSymbol symbol in cells)
+            {
+                if (symbol == PlayerSymbol.None)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
